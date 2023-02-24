735500761f20d281317433a5ef2afa4c6b235fea
Vadim Mokrushin <mokr.vo@yandex.ru>
2023-02-24 17:51:03 +0700

    Password storage converted to a hash

--- users/index.js
+++ users/index.js
@@ -2,10 +2,7 @@ const router = require("express").Router();
 const { userAccounts } = require("./user-accounts");
 
 router.post("/", (request, response, next) => {
-  const user = userAccounts.login(
-    request.body.username,
-    request.body.passwordHash
-  );
+  const user = userAccounts.login(request.body.username, request.body.password);
 
   setTimeout(() => {
     if (!user) response.status(404).send("Неверный пароль или логин");
--- users/user-accounts.js
+++ users/user-accounts.js
@@ -1,10 +1,11 @@
+const bcrypt = require("bcrypt");
+
 class UserAccounts {
   data = [
     {
       id: "496b62cb-2052-4410-b1c0-467a9aa257c0",
-      passwordHash: "metal",
-      // passwordHash:
-      //   "$2a$10$6TdhLfMMM/hbyPQ9r4E.Bu/eIdmx1HKB8YEeI63AaS7CBKmArqBLm",
+      passwordHash:
+        "$2a$10$6TdhLfMMM/hbyPQ9r4E.Bu/eIdmx1HKB8YEeI63AaS7CBKmArqBLm",
       username: "xvadim",
       avatar: "/images/test/vadim.jpg",
       name: "Вадим",
@@ -13,10 +14,14 @@ class UserAccounts {
     },
   ];
 
-  login(username, passwordHash) {
-    const response = this.data.find((user) => user.username === username);
-    if (response?.passwordHash === passwordHash) return response;
-    return undefined;
+  login(username, password) {
+    const accountInformation = this.data.find(
+      (user) => user.username === username
+    );
+    if (!accountInformation) return undefined;
+    if (bcrypt.compareSync(password, accountInformation.passwordHash))
+      return accountInformation;
+    else return undefined;
   }
 }
 
