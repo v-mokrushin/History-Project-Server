36bce035e2b8efd0ee1600e99b43666e3b8d355e
Vadim Mokrushin <mokr.vo@yandex.ru>
2023-02-26 18:23:29 +0700

    Added docker config

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
 const express = require("express");
+var fs = require("fs");
+const https = require("https");
 const app = express();
-const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const articles = require("./articles");
 const users = require("./users");
 
-const port = 3001;
-
-app.use(cors());
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
+app.use(cors({ origin: true }));
+app.options("*", cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -24,9 +19,10 @@ app.use("/articles", articles);
 app.use("/users", users);
 app.use("/public", express.static("public"));
 
-app.listen(port, "localhost", function (err) {
-  if (err) {
-    console.log(err);
-    return;
-  }
-});
+// const options = {
+//   cert: fs.readFileSync("./sslcert/fullchain.pem"),
+//   key: fs.readFileSync("./sslcert/privkey.pem"),
+// };
+
+app.listen(8080);
+// https.createServer(options, app).listen(8443);
